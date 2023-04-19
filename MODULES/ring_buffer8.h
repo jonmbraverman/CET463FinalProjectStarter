a23415a8dc9225b1624d149fb260698156874cda
CCSUCET463 <bravermanj@ccsu.edu>
2023-04-19 11:52:00 -0400

    fixed 8b ring_buffer

--- MODULES/ring_buffer8.h
+++ MODULES/ring_buffer8.h
@@ -22,8 +22,8 @@ typedef enum {FALSE = 0, TRUE = 1} bool;
 
 bool ringbuffer8b_isfull(RingBuffer8b_TypeDef *rb);
 bool ringbuffer8b_isempty(RingBuffer8b_TypeDef *rb);
-unsigned int ringbuffer8b_dequeue(RingBuffer8b_TypeDef *rb);
-bool ringbuffer8b_enqueue(RingBuffer8b_TypeDef *rb, unsigned int bytedata);
+unsigned char ringbuffer8b_dequeue(RingBuffer8b_TypeDef *rb);
+bool ringbuffer8b_enqueue(RingBuffer8b_TypeDef *rb, unsigned char bytedata);
 void ringbuffer8b_flush1(RingBuffer8b_TypeDef *rb);
 
 
